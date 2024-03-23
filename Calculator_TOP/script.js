d08f5cb91d112b3287fec8e20f85cdc4f32fa8f7
codeninja79 <oroborosrandy@gmail.com>
2024-03-23 22:25:30 +0530

    Finished making a functional calculator, only have to change the UI a little

--- Calculator_TOP/script.js
+++ Calculator_TOP/script.js
@@ -1,51 +1,87 @@
-function addition(a,b){
-    return a+b;
-}
-function subtraction (a,b){
-    return a-b;
-}
-function division(a,b){
-    return a/b;
-}
-function multiplication (a,b){
-    return a*b;
+let previousVal = '';
+let currentVal = '';
+let operation = '';
+
+document.addEventListener("DOMContentLoaded", function(){
+    let clearButton = document.querySelector(".clear");
+    let operationButton = document.querySelectorAll(".operation");
+    let resultButton = document.querySelector(".result");
+    let numberButtons = document.querySelectorAll(".number");
+    let decimalButton = document.querySelector(".decimal")
+
+    let previousNum = document.querySelector(".firstNum");
+    let currentNum = document.querySelector(".secondNum");
+    
+    numberButtons.forEach((num) => num.addEventListener("click", function(e){
+        numberButtonDisplay(e.target.textContent);
+        currentNum.textContent = currentVal;
+    })) 
+
+    operationButton.forEach((opr) => opr.addEventListener("click", function(e){
+        operatorButtonDisplay(e.target.textContent);
+        previousNum.textContent = previousVal + " " + operation;
+        currentNum.textContent = currentVal;
+        
+    }))
+
+    clearButton.addEventListener("click", function(){
+        previousVal = '';
+        currentVal = '';
+        operation = '';
+        previousNum.textContent = '';
+        currentNum.textContent = '';
+    })
+    
+    resultButton.addEventListener("click", function(){
+    if(currentVal!='' && previousVal!=''){
+        equalsButtonDisplay();
+        previousNum.textContent = '';
+        if(previousVal.length<=10)
+            currentNum.textContent = previousVal;
+        else
+            currentNum.textContent = previousVal.slice(0,5) + '...'
+ } })
+
+    decimalButton.addEventListener("click", function(){
+        if(!currentVal.includes("."))
+            currentVal += ".";
+    })
+})
+
+function numberButtonDisplay(numbers){
+    if(currentVal.length<= 10){
+    currentVal+=numbers;
+    }
 }
 
-function operation (operator, firstNum,secondNum){
+function operatorButtonDisplay(operator){
+    operation = operator;
+    previousVal = currentVal;
+    currentVal = '';
+
+}
+
+function equalsButtonDisplay(){
+    previousVal = Number(previousVal);
+    currentVal = Number(currentVal);
+
+    if(operation=='+')
+        previousVal+=currentVal;
+    else if(operation == '-')
+        previousVal-=currentVal;
+    else if(operation == '/')
+        previousVal/=currentVal;
+    else if(operation == 'x')
+        previousVal *=currentVal;
+
+    previousVal = Math.round(previousVal * 1000)/1000;
    
-    if (operator=='+'){
-        return addition(firstNum,secondNum);
-    }
-    else if(operator=='-'){
-        return subtraction(firstNum,secondNum);
-    }
-    else if (operator == '/'){
-        return division(firstNum,secondNum);
-    }
-    else if (operator == '*'){
-        return multiplication(firstNum,secondNum);
-    }
+    previousVal = previousVal.toString();
+   
+    currentVal = previousVal.toString();
 }
 
-function numberButton(el){
 
-     document.getElementById("numberArea").value = el.innerHTML;
-     let buttonValue=document.getElementById("numberArea").value;
-}
-
-let firstNum = numberButton(5);
-console.log(firstNum);
-
-// function operatorButton(){
-// if(opr=='+' || opr=='-' || opr=='/' || opr=='*'){
-//     let num1 = parseInt(prompt('Input first number'));
-//     let num2 = parseInt(prompt('Input second number'));
-//     console.log(operation(opr,num1,num2));
-// }
-// else if(opr!='+' || opr!='-' || opr!='/' || opr!='*'){
-//         alert('wrong input, please try again!')
-//         location.reload();
-//     }
 
 
 
